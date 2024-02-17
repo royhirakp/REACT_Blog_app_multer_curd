6b3b0ac4f63ff6ac49ffc927c5ab0d42a5f99af2
Hirak Roy <royhirakp@gmail.com>
2024-02-17 13:07:24 +0530

    error fix

--- src/components/card/Blog.js
+++ src/components/card/Blog.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import './Blog.css'
-import ReadmoreLess from './ReadmoreLess'
+import React from "react";
+import "./Blog.css";
 
-const Blog=(props)=>{
-    const{title,date, describtion} = props.data   
-    // console.log(props.data.image.data.data) 
+const Blog = (props) => {
+  const { title, date, describtion } = props.data;
+  // console.log(props.data.image.data.data)
 
-    const base64Str = btoa(
-        String.fromCharCode(...new Uint8Array((props.data.image?.data.data)))
-    )
+  const base64Str = btoa(
+    String.fromCharCode(...new Uint8Array(props.data.image?.data.data))
+  );
 
-    return (
-        <div className='Blog-container'>
-            <div>
-                <img src={`data:image/png;base64,${base64Str}`} alt="shsh" />
-            </div>
-            <h2>blog</h2>
-            <h3>title:{title}</h3>
-                 <>describtion:
-            {/* <ReadmoreLess>     */}
-            {describtion} 
-            {/* </ReadmoreLess>      */}
-            </> 
-         <p>Date: {date}</p>   
+  return (
+    <div className="Blog-container">
+      <div>
+        <img src={`data:image/png;base64,${base64Str}`} alt="shsh" />
       </div>
-    )
-}
+      <h2>blog</h2>
+      <h3>title:{title}</h3>
+      <>
+        describtion:
+        {/* <ReadmoreLess>     */}
+        {describtion}
+        {/* </ReadmoreLess>      */}
+      </>
+      <p>Date: {date}</p>
+    </div>
+  );
+};
 
 export default Blog;
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,85 +1,112 @@
-import axios from 'axios';
-import React from 'react'
+import axios from "axios";
+import React from "react";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 // import { Navigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import Loader from '../card/Loder';
+import Loader from "../card/Loder";
 // import Coockie from 'js-coocki'
 
 const Login = (props) => {
-  const [inputEmail, setInputaEmail] = useState('')
-  const [inputPassword, setInputaPassword] = useState('')
-  const navgate = useNavigate()
-  const [errorMessege, setErrorMessege] = useState('')
-  const [loader, setLoder] = useState(false)
-  const [checkboxStatus, setCheckboxStatus] = useState(null)
-  const [emailValidaion,setEmailValidation] = useState(false)
-  const [passWValidaion,setPassWValidation] = useState(false)
- 
-  //email and password validation 
-  useEffect(()=>{
-    if(/[0-9]/.test(inputPassword) && /[~!@#$%^&*]/.test(inputPassword))  setPassWValidation(false)
-    else if( inputPassword.length > 2)      setPassWValidation(true)
-    else if(inputPassword.length === 0)      setPassWValidation(false)
+  const [inputEmail, setInputaEmail] = useState("");
+  const [inputPassword, setInputaPassword] = useState("");
+  const navgate = useNavigate();
+  const [errorMessege, setErrorMessege] = useState("");
+  const [loader, setLoder] = useState(false);
+  const [checkboxStatus, setCheckboxStatus] = useState(null);
+  const [emailValidaion, setEmailValidation] = useState(false);
+  const [passWValidaion, setPassWValidation] = useState(false);
 
-    if(/[@]/.test(inputEmail) && /[.]/.test(inputEmail))   setEmailValidation(false)
-    else if(inputEmail.length === 0) setEmailValidation(false)
-    else  setEmailValidation(true)    
-  },[inputEmail,inputPassword])
+  //email and password validation
+  useEffect(() => {
+    if (/[0-9]/.test(inputPassword) && /[~!@#$%^&*]/.test(inputPassword))
+      setPassWValidation(false);
+    else if (inputPassword.length > 2) setPassWValidation(true);
+    else if (inputPassword.length === 0) setPassWValidation(false);
 
-  //Onsubmit function 
+    if (/[@]/.test(inputEmail) && /[.]/.test(inputEmail))
+      setEmailValidation(false);
+    else if (inputEmail.length === 0) setEmailValidation(false);
+    else setEmailValidation(true);
+  }, [inputEmail, inputPassword]);
+
+  //Onsubmit function
   async function onsunmitFun(e) {
-    e.preventDefault()
+    e.preventDefault();
     // console.log('onsubmit')
     // console.log(checkboxStatus)
-    setLoder(true)
+    setLoder(true);
     try {
       const body = {
         email: inputEmail,
-        password: inputPassword
+        password: inputPassword,
+      };
+      if (!checkboxStatus) {
+        alert("please click the checkbox");
+        setLoder(false);
+
+        return;
       }
-      let res = await axios.post('https://hirakp-fullstackblog-post-prt1.onrender.com/user/login', body)
-      setErrorMessege('')
-      console.log(res.data)
-      localStorage.setItem('logToken', res.data.token) 
-      navgate('./home')
+      let res = await axios.post(
+        "https://hirakp-fullstackblog-post-prt1.onrender.com/user/login",
+        body
+      );
+      setErrorMessege("");
+      console.log(res.data);
+      localStorage.setItem("logToken", res.data.token);
+      navgate("./home");
       // // coocki
       // document.cookie = 'SEmail'+inputEmail+";path=http://localhost:3000"
       // document.cookie = 'Spassword'+inputPassword+";path=http://localhost:3000"
     } catch (error) {
-      setErrorMessege(error.response.data.status)
+      setErrorMessege(error.response.data.status);
     }
-    setLoder(false)
+    setLoder(false);
   }
 
   return (
-    <div className='Login-container'>
+    <div className="Login-container">
       <h3>LOGIN</h3>
       <form onSubmit={onsunmitFun}>
         email:
-        <input type='email' style={{border:emailValidaion?"red 4px solid":''}}
+        <input
+          type="email"
+          style={{ border: emailValidaion ? "red 4px solid" : "" }}
           onChange={(e) => setInputaEmail(e.target.value)}
-        /><br/>
-        {
-          emailValidaion ? <p style={{color:"red"}}>not a valid email</p>: <></>
-        }
+        />
+        <br />
+        {emailValidaion ? (
+          <p style={{ color: "red" }}>not a valid email</p>
+        ) : (
+          <></>
+        )}
         password:
-        <input type='password' style={{border:passWValidaion ?"red 4px solid":""}}
+        <input
+          type="password"
+          style={{ border: passWValidaion ? "red 4px solid" : "" }}
           onChange={(e) => setInputaPassword(e.target.value)}
-        /><br/>
-        {          passWValidaion ? <p style={{color:"red"}}>not a valid password</p>:<></>        }
+        />
+        <br />
+        {passWValidaion ? (
+          <p style={{ color: "red" }}>not a valid password</p>
+        ) : (
+          <></>
+        )}
         <button type="submit">Login</button>
       </form>
-      <input type='checkbox'
-        onClick={(e)=>{setCheckboxStatus(e.target.checked)}}
-      /> Remember me? <br />
+      <input
+        type="checkbox"
+        onClick={(e) => {
+          setCheckboxStatus(e.target.checked);
+        }}
+      />{" "}
+      Remember me? <br />
       Need an accout? <Link to="/register">SingUp</Link>
-      {errorMessege ? <h3 style={{ color: 'red' }}>{errorMessege}</h3> : ""}
+      {errorMessege ? <h3 style={{ color: "red" }}>{errorMessege}</h3> : ""}
       {loader ? <Loader /> : <></>}
       {/* <Loader/> */}
     </div>
-  )
-}
+  );
+};
 
 export default Login;
